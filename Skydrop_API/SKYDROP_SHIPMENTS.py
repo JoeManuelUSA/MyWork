2317800335a8c2c4f020d2581d53e7611ea891ae
JoeManuelGAON <jvilleda@gaon.com.mx>
2023-07-31 17:18:20 -0600

    July 31 2023

--- Skydrop_API/SKYDROP_SHIPMENTS.py
+++ Skydrop_API/SKYDROP_SHIPMENTS.py
@@ -1,6 +1,7 @@
+#this code obtains shipments from Skydrop API and stores the information to Mongo DB
 from datetime import datetime, timedelta
 from pymongo import MongoClient
-from dotenv import load_dotenv, set_key
+from dotenv import load_dotenv
 from dateutil.parser import parse
 import requests
 import os
@@ -8,23 +9,19 @@ import os
 load_dotenv()
 #Obtain the Mongo Conection String and DB
 MongoOnlineConnection=os.environ.get('MongoOnlineConnectionString')
-MongoLocalHost=os.environ.get('MongoLocalConnectionHost')
-MongoLocalServer=os.environ.get('MongoLocalConnectionServer')
-#Select Local Db or Online Cluster
 client = MongoClient(MongoOnlineConnection)
-#client=MongoClient(f'{MongoLocalHost}', int(MongoLocalServer))
-#Obtian Credentials
+#Obtain Credentials
 access_token=os.environ.get('SKYDROP_ACCESS_TOKEN')
 cnn = client["GAON_SKYDROP"]
 def obtain_shipments(cnn,access_token):#Function for Obtaining Shipments from Skydrop API
-    collection = cnn["Shipments"]
-    url="https://radar-api.skydropx.com/v1/shipments"#Url
+    collection = cnn["Shipments"]#Selects the collection from Mongo DB
+    url="https://radar-api.skydropx.com/v1/shipments"#Request URL
     #Header for authorization token
     headers = {
         "Authorization": f"Token token={access_token}",
         "content-type": "application/json"
     }
-    response = requests.get(url, headers=headers)#Get request
+    response = requests.get(url, headers=headers)#Executes GET request
     orders = response.json()#Converting the response to JSON format
     page_max=orders['meta']['total_pages']#Consulting Page limit for while condition
     page=0
@@ -52,4 +49,5 @@ def obtain_shipments(cnn,access_token):#Function for Obtaining Shipments from Sk
         url = f"https://radar-api.skydropx.com/v1/shipments?page={page + 1}"
         response = requests.get(url, headers=headers)
         orders = response.json()
+
 obtain_shipments(cnn,access_token)
